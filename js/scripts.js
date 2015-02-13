acb6dd077c91c075b08980f0c3406aefde2a3eef
Lizzie <lizziefkoehler@gmail.com>
2015-02-13 09:57:11 -0800

    add functionality to tell what type of triangle it is

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,19 @@
 var triangle = function(sideOne, sideTwo, sideThree) {
-
-
-});
+	
+	if (sideOne + sideTwo <= sideThree ||
+		sideTwo + sideThree <= sideOne ||
+		sideThree + sideOne <= sideTwo) {
+		return "not a valid triangle";
+	}
+	else if (sideOne === sideTwo  && sideTwo === sideThree) {
+		return "equilateral";
+	}
+	else if (sideOne === sideTwo && sideOne !== sideThree ||
+		sideTwo === sideThree && sideTwo !== sideOne ||
+		sideThree === sideOne && sideThree !== sideOne) {
+		return "isosceles";
+	}
+	else if (sideOne !== sideTwo && sideOne !== sideThree) {
+		return "scalene"
+	}
+};
--- spec/specs.js
+++ spec/specs.js
@@ -1,3 +1,14 @@
 describe('triangle', function() {
-
+	it("tells if all three sides are equal", function() {
+		expect(triangle(3, 3, 3)).to.equal("equilateral");
+	});
+	it("tells if the triangle is isosceles", function () {
+		expect(triangle(5, 5, 2)).to.equal("isosceles");
+	});
+	it("tells if the triangle is a scalene", function () {
+		expect(triangle(5, 7, 8)).to.equal("scalene");
+	});
+	it("tells if its not a valid triangle", function () {
+		expect(triangle(2, 2, 8)).to.equal("not a valid triangle");
+	});
 });
